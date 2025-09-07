94ccfe6e05e6d2ac9eb61731929ce041ce1f3a74
Ujjwal Kandel <ujjwalkandel1@gmai.com>
2025-09-07 23:45:02 +0545

    Home

--- src/components/home/HomeHeroText.jsx
+++ src/components/home/HomeHeroText.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
+import Video from './video'
 
 const HomeHeroText = () => {
   return (
-    <div className='font-[font2] '>
+    <div className='font-[font2] pt-5 text-center'>
         <div className='text-[7vw] uppercase leading-[10vw] '>Hi, I am</div>
 
-        <div className='text-[7vw] uppercase leading-[10vw] '>Frontend Developer</div>
+        <div className='text-[7vw] flex items-center uppercase leading-[10vw] '>Frontend 
+            <div className='h-'>
+            <Video /> 
+            </div>
+            Developer
+            </div>
 
-        <div className='text-[7vw] uppercase leading-[10vw] t'>Backend Developer</div>
+        <div className='text-[7vw] uppercase leading-[10vw] t'>Qui genere</div>
     </div>
   )
 }
